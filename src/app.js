24d67ae567a9872ebc4373bd65fa1f63d5dbd1dd
Renan Damascena Pereira <renandamascena@gmail.com>
2023-01-13 21:41:01 -0300

    modificações no post participants

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import Joi from "joi"
 import { MongoClient, ObjectId } from "mongodb"
 
 dotenv.config()
@@ -29,7 +30,33 @@ catch{(err) => console.log(err)}
 
 
 
-app.post("/participants",(req,res) => {
+app.post("/participants", async (req,res) => {
+
+    const participant = req.body
+
+    const participantSchema = Joi.object({
+        name:Joi.string().required()
+    })
+
+    const validation = participantSchema.validate(participant)
+
+    if(validation.error){
+    
+        const errors = validation.error.details.map((d) => d.message)
+        
+        return res.status(422).send(errors)
+    }
+
+    try{
+
+        // const operation = await db.collection('UOL').find
+
+    }
+    catch{
+        console.log("Algo deu errado")
+    }
+
+
 
 })
 
@@ -41,11 +68,16 @@ app.post("/messages",(req,res) => {
     
 })
 
-app.get("/messages",(req,res) => {
+app.get("/messages", async (req,res) => {
 
-    db.collection("UOL").find().toArray().then((m) => {
-        return res.send(m)
-    })
+   try{
+       const messages =  await db.collection("UOL").find().toArray() 
+
+       res.send(messages)
+   }
+   catch{
+    res.send("algo deu errado")
+   }
     
 })
 
